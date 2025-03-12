ad0774c9a2039fa23a31c80e1953d16f595c6e70
Ramaaditia123 <adhytt0@gmail.com>
2025-03-12 14:55:08 +0700

    Add files via upload

---
+++ arayy.php
@@ -0,0 +1,11 @@
+<?php 
+#temanSaya = ["Gagak", "Jamet", "Alay"];
+print_r($temanSaya); //untuk menampilkan seluruh nilai array 
+echo $temanSaya[2];
+for ($i=0; $i < 5;)
+while
+do {}
+?>
+<?php 
+    
+ ?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+echo"Selamat Siang <br>";
+$NamaDepan="Rama";
+$NamaBelakang="Aditia";               
+echo "Nama : ".$NamaDepan." ".$NamaBelakang."<br>";
+
+echo"<br> <br> ---------- UJI COBA ----------<br>";
+$NamaDepan2= "Rama Aditia";
+echo $NamaDepan2."<br>";
+// $Umur="18 Tahun";
+// $Hobi="Tidur dan Ngegame";
+
+define("PHI", 3.14);
+echo PHI."<br>";
+$jari = 10;
+$luas = PHI * $jari ** 2;
+echo "Luas Lingkarannya adalah $luas"
+ ?>
+<?php 
+echo"<br> <br> ---------- UJI COBA ----------<br>";
+$NamaDepan2= "Rama Aditia";
+echo $NamaDepan2."<br>";
+ ?>
